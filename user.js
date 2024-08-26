e49249990b6a9a2c7c3cc5e76278edaf00930e93
delicious1341 <130315844+delicious1341@users.noreply.github.com>
2024-08-26 15:56:21 +0000

    All files other than chrome directory uploaded
    Plan to see if pdf viewer css hacks available before implementing them into userChrome.css or userContent.css

---
+++ user.js
@@ -0,0 +1,5 @@
+// Enable use of custom userChrome.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Fullscreen constricted to window size
+user_pref("browser.toolbars.bookmarks.visibility", "never");
