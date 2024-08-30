166824a723e9287b5885fb01144c778fa6eada28
delicious1341 <130315844+delicious1341@users.noreply.github.com>
2024-08-30 21:52:57 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+sudo apt update && sudo apt install -y wget gnupg apt-transport-https ca-certificates
+
+wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
+
+sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
+Types: deb
+URIs: https://deb.librewolf.net
+Suites: bookworm
+Components: main
+Architectures: amd64
+Signed-By: /usr/share/keyrings/librewolf.gpg
+EOF
+
+sudo apt update
+
+sudo apt install librewolf -y
