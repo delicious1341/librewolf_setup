00e509a34b36bb04b6765faad4f3e1583f26c3f9
delicious1341 <130315844+delicious1341@users.noreply.github.com>
2024-08-30 21:47:16 +0000

    Add files via upload

--- user.js
+++ user.js
@@ -3,3 +3,10 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
 // Never show bookmarks bar
 user_pref("browser.toolbars.bookmarks.visibility", "never");
+
+//
+user_pref("privacy.resistFingerprinting","true");
+user_pref("privacy.fingerprintingProtection","true");
+//user_pref("privacy.fingerprintingProtection.overrides","+AllTargets,-CSSPrefersColorScheme");
+user_pref("ui.systemUsesDarkTheme", 1);
+user_pref("browser.in-content.dark-mode", true);
