a1802823ed587776ce66ba52a8d980c11f52e2e1
delicious1341 <130315844+delicious1341@users.noreply.github.com>
2024-08-30 23:41:27 +0100

    Update user.js

--- user.js
+++ user.js
@@ -4,10 +4,10 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 // Never show bookmarks bar
 user_pref("browser.toolbars.bookmarks.visibility", "never");
 
-// Allow fingerprinting to request dark mode
-//user_pref("privacy.resistFingerprinting","false");
-//user_pref("privacy.fingerprintingProtection","true");
-//user_pref("privacy.fingerprintingProtection.overrides","+AllTargets,-CSSPrefersColorScheme");
+// Allow fingerprinting to request dark mode. Set dark mode manually after Ctrl+Shift+O.
+user_pref("privacy.resistFingerprinting", false);
+user_pref("privacy.fingerprintingProtection", true);
+user_pref("privacy.fingerprintingProtection.overrides","+AllTargets,-CSSPrefersColorScheme");
 
 // Seting dark mode automatically (not as good as doing it manually)
 //user_pref("ui.systemUsesDarkTheme", 1);
