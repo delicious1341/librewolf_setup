1d8a357136232a7ce010e3c0122da9fd6783df05
delicious1341 <130315844+delicious1341@users.noreply.github.com>
2024-08-30 21:51:11 +0000

    Update user.js

--- user.js
+++ user.js
@@ -4,9 +4,11 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 // Never show bookmarks bar
 user_pref("browser.toolbars.bookmarks.visibility", "never");
 
-//
-user_pref("privacy.resistFingerprinting","false");
-user_pref("privacy.fingerprintingProtection","true");
-user_pref("privacy.fingerprintingProtection.overrides","+AllTargets,-CSSPrefersColorScheme");
-user_pref("ui.systemUsesDarkTheme", 1);
-user_pref("browser.in-content.dark-mode", true);
+// Allow fingerprinting to request dark mode
+//user_pref("privacy.resistFingerprinting","false");
+//user_pref("privacy.fingerprintingProtection","true");
+//user_pref("privacy.fingerprintingProtection.overrides","+AllTargets,-CSSPrefersColorScheme");
+
+// Seting dark mode automatically (not as good as doing it manually)
+//user_pref("ui.systemUsesDarkTheme", 1);
+//user_pref("browser.in-content.dark-mode", true);
